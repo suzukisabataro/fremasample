9c8765228a06c9a54e5583eb953666ff85ae2097
suzukisabataro <dwdwdwtjagpmdwdwx@gmail.com>
2020-05-05 21:40:10 +0900

    third

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,51 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.13.0"
+lock "3.13.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+
+set :application, 'freemarket_sample_74b'
+set :repo_url,  'git@github.com:suzukisabataro/freemarket_sample_74b.git'
+
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+# chat-spaceで使ったpemを指定
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/sabataro.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+set :linked_files, %w{ config/master.key }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
+
+
+
+
+# set :application, ""
+# set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -37,39 +80,37 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-set :application, 'freemarket_sample_74b'
-set :repo_url,  'git@github.com:suzukisabataro/freemarket_sample_74b.git'
+# set :application, 'freemarket_sample_74b'
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+# set :rbenv_type, :user
+# set :rbenv_ruby, '2.5.1'
 
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/sabataro.pem']
+# set :ssh_options, auth_methods: ['publickey'],
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# set :keep_releases, 5
 
-set :linked_files, %w{ config/master.key }
+# set :linked_files, %w{ config/master.key }
 
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
-  end
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:stop'
+#     invoke 'unicorn:start'
+#   end
 
-  desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
-end
+#   desc 'upload master.key'
+#   task :upload do
+#     on roles(:app) do |host|
+#       if test "[ ! -d #{shared_path}/config ]"
+#         execute "mkdir -p #{shared_path}/config"
+#       end
+#       upload!('config/master.key', "#{shared_path}/config/master.key")
+#     end
+#   end
+#   before :starting, 'deploy:upload'
+#   after :finishing, 'deploy:cleanup'
+# end
