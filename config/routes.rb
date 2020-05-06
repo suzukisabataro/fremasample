ef3ce8520989507ec9a2b8bebd614589d928f30b
suzukisabataro <dwdwdwtjagpmdwdwx@gmail.com>
2020-05-06 14:59:05 +0900

    8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root 'items#index' 
+  resources :items, only: [ :show, :edit, :new]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+ 
