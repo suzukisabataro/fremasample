0c5fbbe1c4f79673f04f6c935b7ebca62f48ad10
suzukisabataro <dwdwdwtjagpmdwdwx@gmail.com>
2020-05-11 13:41:39 +0900

    投稿機能実装
    メッセージ投稿機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.item_images.build
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
   end
 
   def edit
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, item_images_attributes: [:image])
   end
 end
